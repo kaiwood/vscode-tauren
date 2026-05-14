8f6a7b5c646995277f1461387fec73b28e779f8d
Kai Wood <kai.wood@icloud.com>
2026-05-14 23:36:40 +0200

    Improve nonce creation

--- src/nonce.ts
+++ src/nonce.ts
@@ -1,10 +1,13 @@
+import { randomInt } from 'crypto';
+
 const nonceChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const nonceLength = 32;
 
 export function createNonce(): string {
   let nonce = '';
 
-  for (let index = 0; index < 32; index += 1) {
-    nonce += nonceChars.charAt(Math.floor(Math.random() * nonceChars.length));
+  for (let index = 0; index < nonceLength; index += 1) {
+    nonce += nonceChars[randomInt(nonceChars.length)];
   }
 
   return nonce;
